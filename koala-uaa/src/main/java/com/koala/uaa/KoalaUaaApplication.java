b5711d79134806cc1720f86f34816a2372a894dd
zhaowei <zhaowei@huimin100.cn>
2021-02-19 09:44:03 +0800

    first

---
+++ koala-uaa/src/main/java/com/koala/uaa/KoalaUaaApplication.java
@@ -0,0 +1,13 @@
+package com.koala.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoalaUaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoalaUaaApplication.class, args);
+	}
+
+}
---
+++ koala-uaa/src/test/java/com/koala/uaa/KoalaUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.uaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoalaUaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
