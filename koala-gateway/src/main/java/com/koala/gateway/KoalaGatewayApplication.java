0359e18b77cb4c0ae739d6cb3ea1903938cb9485
zhaowei <zhaowei@huimin100.cn>
2021-03-03 10:39:59 +0800

    初始化网关代码

---
+++ koala-gateway/src/main/java/com/koala/gateway/KoalaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.koala.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoalaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoalaGatewayApplication.class, args);
+	}
+
+}
---
+++ koala-gateway/src/test/java/com/koala/gateway/KoalaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoalaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
